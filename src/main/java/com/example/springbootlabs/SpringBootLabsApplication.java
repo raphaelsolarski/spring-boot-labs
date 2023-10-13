9345a5a3e050c2ed441b538fd19b66e119be8ff6
Rafał Solarski <solarski.rafal@gmail.com>
2023-10-13 19:24:43 +0200

    Actuator added

--- src/main/java/com/example/springbootlabs/SpringBootLabsApplication.java
+++ src/main/java/com/example/springbootlabs/SpringBootLabsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootLabsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLabsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLabsApplication.class, args);
+    }
 
 }
