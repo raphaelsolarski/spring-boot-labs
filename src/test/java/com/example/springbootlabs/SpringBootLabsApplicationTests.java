018e604f936aa6ac82b24e920b2a2ed0ee04ecdf
Rafał Solarski <solarski.rafal@gmail.com>
2023-10-13 19:21:35 +0200

    Unpacked package from initializ

---
+++ src/test/java/com/example/springbootlabs/SpringBootLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
