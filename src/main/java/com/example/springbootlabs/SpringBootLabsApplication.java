7c28df7cd5a82b1510cec1ccb56fbab9217603c8
Sebastian Kowalczyk <s17513@pjwstk.edu.pl>
2023-10-13 22:08:29 +0200

    wprowadzenie

--- src/main/java/com/example/springbootlabs/SpringBootLabsApplication.java
+++ src/main/java/com/example/springbootlabs/SpringBootLabsApplication.java
@@ -1,13 +1,27 @@
 package com.example.springbootlabs;
 
+import com.example.springbootlabs.product.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SpringBootLabsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLabsApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Product> hello(){
+		return List.of(
+				new Product(1L,"Biurko Narozne","Biurko z szufladą na klawiaturę",550.0),
+				new Product(1L,"Krzesło","Krzesło bez podłokietników",120.0)
+		);
 	}
 
 }
