e921e34a6d81337405fe86471c51be1f7e5e4586
Rafał Solarski <solarski.rafal@gmail.com>
2023-10-15 09:01:20 +0200

    Conflicting beans to autowire

--- src/main/java/com/example/springbootlabs/product/ProductService.java
+++ src/main/java/com/example/springbootlabs/product/ProductService.java
@@ -9,8 +9,8 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductService(ProductRepository someProductRepository) {
+        this.productRepository = someProductRepository;
     }
 
     public List<Product> getProducts() {
