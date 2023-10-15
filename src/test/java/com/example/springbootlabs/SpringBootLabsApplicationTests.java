90447379c88a38b13d6f458a32a2be8ce5a0af69
Rafał Solarski <solarski.rafal@gmail.com>
2023-10-15 09:06:00 +0200

    Activating profiles in tests

--- src/test/java/com/example/springbootlabs/SpringBootLabsApplicationTests.java
+++ src/test/java/com/example/springbootlabs/SpringBootLabsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.springbootlabs;
 
+import com.example.springbootlabs.product.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBootLabsApplicationTests {
+
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Test
 	void contextLoads() {
+		productRepository.getProducts();
 	}
 
 }
