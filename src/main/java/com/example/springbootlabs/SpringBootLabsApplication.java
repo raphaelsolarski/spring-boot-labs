d512fcaf9b34d3ad091201320324fd1e5433a95c
Rafał Solarski <solarski.rafal@gmail.com>
2023-10-15 08:34:56 +0200

    ProductRepository rewritten using @Bean

--- src/main/java/com/example/springbootlabs/SpringBootLabsApplication.java
+++ src/main/java/com/example/springbootlabs/SpringBootLabsApplication.java
@@ -1,10 +1,17 @@
 package com.example.springbootlabs;
 
+import com.example.springbootlabs.product.ProductRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootLabsApplication {
+
+    @Bean
+    public ProductRepository productRepository() {
+        return new ProductRepository();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootLabsApplication.class, args);
--- src/main/java/com/example/springbootlabs/product/ProductRepository.java
+++ src/main/java/com/example/springbootlabs/product/ProductRepository.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public class ProductRepository {
 
     public List<Product> getProducts() {
