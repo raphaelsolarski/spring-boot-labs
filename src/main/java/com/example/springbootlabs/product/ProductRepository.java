54d6fad9a421972dd81ffd2ff1fb056c467bc589
Rafał Solarski <solarski.rafal@gmail.com>
2023-10-15 08:42:18 +0200

    Revert "Presentation of circular dependency"
    This reverts commit 6fcb849b5b0af7c53f5e8407993b43e8f447bc76.

--- src/main/java/com/example/springbootlabs/product/ProductRepository.java
+++ src/main/java/com/example/springbootlabs/product/ProductRepository.java
@@ -7,12 +7,6 @@ import java.util.List;
 @Repository
 public class ProductRepository {
 
-    private final ProductService productService;
-
-    public ProductRepository(ProductService productService) {
-        this.productService = productService;
-    }
-
     public List<Product> getProducts() {
         return List.of(new Product(1L, "productName", "product desc", 1.0));
     }
