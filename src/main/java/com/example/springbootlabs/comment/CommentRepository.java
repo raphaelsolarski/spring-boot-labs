01b120309f0d37978290928dff0206a414f4ae80
gitMaciek86 <maciej.and.grzelec@gmail.com>
2023-10-29 10:10:30 +0100

    final version

--- src/main/java/com/example/springbootlabs/comment/CommentRepository.java
+++ src/main/java/com/example/springbootlabs/comment/CommentRepository.java
@@ -9,7 +9,22 @@ import java.util.Set;
 import java.util.UUID;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByText(String text);
 
+    List<Comment> findByCommentType(CommentType commentType);
+
+    Optional<Comment> findByUuID(UUID uuID);
+
+    List<Comment> findByTextAndCommentType(String text, CommentType commentType);
+
+    Long countByCommentType(CommentType commentType);
+
+    List<Comment> findByTextContaining(String text);
+    List<Comment> findTop5ByCommentTypeOrderById(CommentType commentType);
+    List<Comment> findByCommentTypeIsNull();
+    Long countByTextContainingAndCommentType(String text, CommentType commentType);
+    @Query("SELECT DISTINCT c.commentType FROM Comment c WHERE c.text LIKE %:text%")
+    Set<CommentType> findDistinctCommentTypesByTextContaining(String text);
 }
 
 
